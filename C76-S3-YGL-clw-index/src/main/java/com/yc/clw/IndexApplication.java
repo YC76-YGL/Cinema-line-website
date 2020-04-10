0482ecab884f3d6045dd6a963952812b89512dbf
yzy-hushuo <1481835274@qq.com>
2020-04-10 21:18:47 +0800

    加入无线加载插件

--- C76-S3-YGL-clw-index/src/main/java/com/yc/clw/IndexApplication.java
+++ C76-S3-YGL-clw-index/src/main/java/com/yc/clw/IndexApplication.java
@@ -1,5 +1,13 @@
 package com.yc.clw;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IndexApplication {
 
+	public static void main(String[] args)
+	{
+		SpringApplication.run(IndexApplication.class, args);
+	}
 }
