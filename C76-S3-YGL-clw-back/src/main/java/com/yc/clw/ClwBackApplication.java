6d13c055e6bc2eb4128671c75169528ed0a319d2
yzy-hushuo <1481835274@qq.com>
2020-04-11 14:23:28 +0800

    依賴影響yml的效用

--- C76-S3-YGL-clw-back/src/main/java/com/yc/clw/ClwBackApplication.java
+++ C76-S3-YGL-clw-back/src/main/java/com/yc/clw/ClwBackApplication.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 //MyBatis 接口组件扫描
 @MapperScan("com.yc.clw")
-@EnableRedisHttpSession
 //开启声明式远程服务调用
 @EnableFeignClients
 public class ClwBackApplication {
