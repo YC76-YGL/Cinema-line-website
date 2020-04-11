d16a557254746fa108fffec02bf0ca8a35cab495
yzy-hushuo <1481835274@qq.com>
2020-04-11 18:51:04 +0800

    index.html的调试

--- C76-S3-YGL-clw-index/src/main/java/com/yc/clw/web/IndexAction.java
+++ C76-S3-YGL-clw-index/src/main/java/com/yc/clw/web/IndexAction.java
@@ -1,11 +1,15 @@
 package com.yc.clw.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yc.clw.bean.ClwGenersExample;
+import com.yc.clw.dao.ClwGenersMapper;
 import com.yc.clw.dao.ClwMovielistMapper;
 
 @RestController
@@ -14,8 +18,15 @@ public class IndexAction {
 	@Resource
 	private ClwMovielistMapper cml;
 	
+	@Resource
+	private ClwGenersMapper cgm;
+	
 	@GetMapping({"/","index","index.html"})
 	public ModelAndView index(ModelAndView mav) {
+		ClwGenersExample cGenersExample = new ClwGenersExample();
+		cGenersExample.createCriteria().andIdEqualTo(1);
+		List<?> generlist = cgm.selectByExample(cGenersExample);
+		mav.addObject("generlist",generlist);
 		mav.setViewName("index");
 		return mav;
 	}
