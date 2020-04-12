837678a69137a929c960d1ea554941cb61cdab66
yzy-hushuo <1481835274@qq.com>
2020-04-12 16:41:29 +0800

    远程式分布系统

--- C76-S3-YGL-clw-index/src/main/java/com/yc/clw/IndexApplication.java
+++ C76-S3-YGL-clw-index/src/main/java/com/yc/clw/IndexApplication.java
@@ -1,13 +1,20 @@
 package com.yc.clw;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @MapperScan("com.yc.clw")
+//开启远程服务调用
+@EnableFeignClients
 public class IndexApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IndexApplication.class, args);
 	}
--- C76-S3-YGL-clw-index/src/main/java/com/yc/clw/web/IndexAction.java
+++ C76-S3-YGL-clw-index/src/main/java/com/yc/clw/web/IndexAction.java
@@ -1,15 +1,11 @@
 package com.yc.clw.web;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.yc.clw.bean.ClwGenersExample;
-import com.yc.clw.dao.ClwGenersMapper;
 import com.yc.clw.dao.ClwMovielistMapper;
 
 @RestController
@@ -19,14 +15,12 @@ public class IndexAction {
 	private ClwMovielistMapper cml;
 	
 	@Resource
-	private ClwGenersMapper cgm;
+	IGenerAction igaAction;
 	
 	@GetMapping({"/","index","index.html"})
 	public ModelAndView index(ModelAndView mav) {
-		ClwGenersExample cGenersExample = new ClwGenersExample();
-		cGenersExample.createCriteria().andIdEqualTo(1);
-		List<?> generlist = cgm.selectByExample(cGenersExample);
-		mav.addObject("generlist",generlist);
+		//通过远程接口方式调用信息
+		mav.addObject("generlist",igaAction.getGeners());
 		mav.setViewName("index");
 		return mav;
 	}
