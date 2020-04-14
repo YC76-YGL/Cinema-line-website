9e6d4c37971aba1ef2ec8878a1411082bdf73754
yzy-hushuo <1481835274@qq.com>
2020-04-14 19:12:48 +0800

    网页的修改

--- C76-S3-YGL-clw-back/src/main/java/com/yc/clw/web/ClwMovieListAction.java
+++ C76-S3-YGL-clw-back/src/main/java/com/yc/clw/web/ClwMovieListAction.java
@@ -1,9 +1,14 @@
 package com.yc.clw.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yc.clw.bean.ClwMovielist;
+import com.yc.clw.bean.ClwMovielistExample;
 import com.yc.clw.dao.ClwMovielistMapper;
 
 @RestController
@@ -11,5 +16,13 @@ public class ClwMovieListAction {
 
 	@Resource
 	private ClwMovielistMapper cmlm;
+	
+	@GetMapping("getClwMovielist")
+	public List<ClwMovielist> getClwMovielist(){
+		ClwMovielistExample cmle =new ClwMovielistExample();
+		cmle.createCriteria().andIdIsNotNull();
+		List<ClwMovielist> list = cmlm.selectByExample(cmle);
+		return list;
+	}
 	
 }
